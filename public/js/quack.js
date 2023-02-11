fd13c94a282a9b377a5e7adb9677975c97026659
abdallahaac <aceydesignz@gmail.com>
2023-02-11 13:42:08 -0500

    update

--- public/js/quack.js
+++ public/js/quack.js
@@ -1,7 +1,6 @@
 AFRAME.registerComponent("quack", {
 	init: function () {
 		let duck = this.el;
-		let audio = document.querySelector("#button1");
 		duck.setAttribute("sound", "src:#quack; on:click");
 	},
 });
